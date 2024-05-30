7ea6e6e74f8ede32c7f83ae4626d3d9ef74334e2
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-30 22:24:00 +0600

    Create nfs-client.sh

---
+++ nfs-client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Install the NFS Client on the Client Systems
+apt update -y
+apt install nfs-common -y
+mkdir -p /nfs-share
+chmod 777 /nfs-share
+mount 192.168.0.96:/nfs-share /nfs-share
+cd /nfs-share
+ls -l
