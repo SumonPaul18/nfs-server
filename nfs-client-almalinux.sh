14be02d26deb6c0261b0d2d024720f9d68196376
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-30 22:37:04 +0600

    Create nfs-client-almalinux.sh

---
+++ nfs-client-almalinux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Configure NFS Client
+dnf -y install nfs-utils
+showmount -e 192.168.0.96
+mount -t nfs -o vers=3 192.168.0.96:/nfs-share /mnt
+cat <<EOF | sudo tee /etc/fstab
+192.168.0.96:/nfs-share  nfs  defaults  0  0
+mount -a
+df -h
+#umount /mnt
