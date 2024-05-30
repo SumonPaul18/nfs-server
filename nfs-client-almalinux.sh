5de94ef172e53e7a771070206ad2e737a3e63ccc
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-30 22:37:59 +0600

    Update nfs-client-almalinux.sh

--- nfs-client-almalinux.sh
+++ nfs-client-almalinux.sh
@@ -3,6 +3,7 @@
 dnf -y install nfs-utils
 showmount -e 192.168.0.96
 mount -t nfs -o vers=3 192.168.0.96:/nfs-share /mnt
+ls -l /mnt
 cat <<EOF | sudo tee /etc/fstab
 192.168.0.96:/nfs-share  nfs  defaults  0  0
 mount -a
