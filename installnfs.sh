5308582c388c2eb4a9cca9b4098465eb8247210a
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-30 22:13:32 +0600

    Create installnfs.sh

---
+++ installnfs.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+#Install NFS Server and Client on Ubuntu
+apt update -y
+apt install nfs-kernel-server -y
+mkdir -p /nfs-share
+chown -R nobody:nogroup /nfs-share
+chmod 777 /nfs-share
+cat <<EOF | sudo tee /etc/exports
+/nfs-share 192.168.0.0/24(rw,sync,no_subtree_check)
+EOF
+exportfs -a
+systemctl restart nfs-kernel-server
