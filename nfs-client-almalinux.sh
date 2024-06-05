ce2c2bd34252993c44ce4d1e70f2f2fb106b800f
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-06-05 14:02:29 +0600

    Update nfs-client-almalinux.sh

--- nfs-client-almalinux.sh
+++ nfs-client-almalinux.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #Configure NFS Client
 dnf -y install nfs-utils
+systemctl enable --now nfs-server
+showmount -e
 showmount -e 192.168.0.96
 mount -t nfs -o vers=3 192.168.0.96:/nfs-share /mnt
 ls -l /mnt
 cp /etc/fstab /etc/fstab.copy
 cat <<EOF | sudo tee -a /etc/fstab
-192.168.0.96:/nfs-share  nfs  defaults  0  0
+192.168.0.96:/nfs-share  /mnt  nfs  defaults  0  0
 EOF
 mount -a
 df -h
-systemctl start rpcbind
-systemctl enable rpcbind
+systemctl enable --now rpcbind
+systemctl restart nfs-server
+
 #umount /mnt
