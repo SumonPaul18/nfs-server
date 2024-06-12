001fc1a99aa08c2f4f034f21d3129a9c8c24624a
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-06-12 14:33:57 +0600

    Update nfs-client-ubuntu.sh

--- nfs-client-ubuntu.sh
+++ nfs-client-ubuntu.sh
@@ -2,6 +2,8 @@
 #Install the NFS Client on the Client Systems
 apt update -y
 apt install nfs-common -y
+systemctl start nfs-common
+systemctl enable nfs-common
 mkdir -p /nfs-share
 chmod 777 /nfs-share
 mount 192.168.0.96:/nfs-share /nfs-share
