435c8be0d50b1c98277281a92afb0bf99255bdab
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-07-11 10:41:10 +0600

    Update nfs-client-ubuntu.sh

--- nfs-client-ubuntu.sh
+++ nfs-client-ubuntu.sh
@@ -7,6 +7,9 @@ systemctl enable nfs-common
 systemctl is-active --quiet nfs-common && echo nfs-common is running
 mkdir -p /nfs-share
 chmod 777 /nfs-share
+cat <<EOF | sudo tee -a /etc/fstab
+192.168.0.96:/nfs-share /nfs-share  nfs      defaults    0       0
+EOF
 mount 192.168.0.96:/nfs-share /nfs-share
 systemctl restart nfs-server.service
 cd /nfs-share
